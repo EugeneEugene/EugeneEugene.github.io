b8255de68a85d18d6093d8a48055ca647829b6a5
EugeneEugene <evgeniiledin@gmail.com>
2019-02-17 21:43:14 +0100

    Remobe garbage

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@ var myImage = document.querySelector('img');
 myImage.onclick = function() {
     var mySrc = myImage.getAttribute('src');
     if(mySrc === 'images/img1.png') {
-      myImage.setAttribute ('src','images/img2.png');
+      myImage.setAttribute ('src','images/img1.png');
     } else {
       myImage.setAttribute ('src','images/img1.png');
     }
@@ -13,18 +13,18 @@ myImage.onclick = function() {
 var myButton = document.querySelector('button');
 var myHeading = document.querySelector('h1'); 
 
-function setUserName() {
-    var myName = prompt('Please enter your name.');
-    localStorage.setItem('name', myName);
-    myHeading.textContent = 'is cool, ' + myName;
-  }
+// function setUserName() {
+//     var myName = prompt('Please enter your name.');
+//     localStorage.setItem('name', myName);
+//     myHeading.textContent = 'is cool, ' + myName;
+//   }
 
-  if(!localStorage.getItem('name')) {
-    setUserName();
-  } else {
-    var storedName = localStorage.getItem('name');
-    myHeading.textContent = 'Mozilla is cool, ' + storedName;
-  }
+  // if(!localStorage.getItem('name')) {
+  //   setUserName();
+  // } else {
+  //   var storedName = localStorage.getItem('name');
+  //   myHeading.textContent = 'Mozilla is cool, ' + storedName;
+  // }
 
   myButton.onclick = function() {
     setUserName();
